ab72e7b1465f64cc90e3a014c6b7f9ffe10170cb
renata turani <reniturani@gmail.com>
2025-12-16 11:01:30 -0300

    Delete database.sql

--- database.sql
+++
@@ -1,6 +0,0 @@
--- Proyecto: Digital Solutions
--- Archivo: database.sql
--- Descripción: Creación de la base de datos empresa y selección del esquema
-DROP DATABASE IF EXISTS empresa ;
-CREATE database empresa; 
-USE empresa ; 
