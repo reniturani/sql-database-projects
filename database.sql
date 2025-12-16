e50558160dec8b6625b61e092cefc3804921df3f
renata turani <reniturani@gmail.com>
2025-12-15 21:20:35 -0300

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,6 @@
+-- Proyecto: Digital Solutions
+-- Archivo: database.sql
+-- Descripción: Creación de la base de datos empresa y selección del esquema
+DROP DATABASE IF EXISTS empresa ;
+CREATE database empresa; 
+USE empresa ; 
